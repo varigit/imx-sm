a1c9565b49eeb5469a3387f8a649ceb436588812
Alexander Herfurtner <alexander.h@variscite.com>
2024-06-25 13:44:15 +0200

    sm: test: scmi: misc: Add EEPROM Xfer test case
    This add EEPROM Xfer test to the SM test mode.
    
    Signed-off-by: Alexander Herfurtner <alexander.h@variscite.com>
    (cherry picked from commit fc09237266313df6c37df4960a577c90dd55de64)
    
    Conflicts:
    	sm/test/scmi/test_scmi_misc.c
    
    Signed-off-by: Alexander Herfurtner <alexander.h@variscite.com>
    (cherry picked from commit ff8724a4af6e08856c5fef82fce497d8cc00f6e7)

--- sm/test/scmi/test_scmi_misc.c
+++ sm/test/scmi/test_scmi_misc.c
@@ -2,6 +2,7 @@
 ** ###################################################################
 **
 ** Copyright 2023-2026 NXP
+** Copyright 2024-2026 Variscite
 **
 ** Redistribution and use in source and binary forms, with or without modification,
 ** are permitted provided that the following conditions are met:
@@ -347,6 +348,36 @@ void TEST_ScmiMisc(void)
             passoverBuffer), SM_ERR_TEST);
         SM_TestModeSet(SM_TEST_MODE_OFF);
 #endif
+    }
+
+    /* Test EEPROM Xfer */
+    {
+        uint8_t devId = 0x52U;
+        uint8_t buffer[64];
+        uint16_t offset = 0U;
+
+        CHECK(SCMI_MiscEepromXfer(SM_TEST_DEFAULT_CHN, devId, SCMI_MISC_EEPROM_XFER_DIR_READ,
+            offset, buffer, sizeof(buffer)));
+
+        /* Branch -- Invalid Channel */
+        NECHECK(SCMI_MiscEepromXfer(SM_SCMI_NUM_CHN, devId, SCMI_MISC_EEPROM_XFER_DIR_READ,
+            offset, buffer, sizeof(buffer)), SCMI_ERR_INVALID_PARAMETERS);
+
+        /* Branch -- Nullpointer */
+        NECHECK(SCMI_MiscEepromXfer(SM_TEST_DEFAULT_CHN, devId, SCMI_MISC_EEPROM_XFER_DIR_READ,
+            offset, NULL, sizeof(buffer)), SCMI_ERR_INVALID_PARAMETERS);
+
+        /* Branch -- Invalid length */
+        NECHECK(SCMI_MiscEepromXfer(SM_TEST_DEFAULT_CHN, devId, SCMI_MISC_EEPROM_XFER_DIR_READ,
+            offset, buffer, 0), SCMI_ERR_INVALID_PARAMETERS);
+
+        /* Branch -- Invalid DevId */
+        NECHECK(SCMI_MiscEepromXfer(SM_TEST_DEFAULT_CHN, 0xFFU, SCMI_MISC_EEPROM_XFER_DIR_READ,
+            offset, buffer, sizeof(buffer)), SCMI_ERR_NOT_FOUND);
+
+        /* Branch -- Invalid direction */
+        NECHECK(SCMI_MiscEepromXfer(SM_TEST_DEFAULT_CHN, devId, 0xFFU,
+            offset, buffer, sizeof(buffer)), SCMI_ERR_INVALID_PARAMETERS);
     }
 
     /* Control Set */
