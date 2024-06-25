469e7ca3e7fc87991fdd68b992b7ec63f14dd7e3
Alexander Herfurtner <alexander.h@variscite.com>
2024-06-25 13:43:48 +0200

    components: scmi: misc: add EEPROM Xfer support
    This adds EEPROM Xfer support (client-side), currently only considered
    to add the servie to the built-in test support.
    
    Signed-off-by: Alexander Herfurtner <alexander.h@variscite.com>
    (cherry picked from commit e3f53b00251f3611c4badac8e67af69d1d498625)
    
    Conflicts:
    	components/scmi/scmi_misc.h

--- components/scmi/scmi_misc.c
+++ components/scmi/scmi_misc.c
@@ -2,6 +2,7 @@
 ** ###################################################################
 **
 ** Copyright 2023-2024 NXP
+** Copyright 2024 Variscite
 **
 ** Redistribution and use in source and binary forms, with or without modification,
 ** are permitted provided that the following conditions are met:
@@ -930,3 +931,60 @@ int32_t SCMI_MiscControlEvent(uint32_t channel, uint32_t *ctrlId,
     return status;
 }
 
+/*--------------------------------------------------------------------------*/
+/* Perform EEPROM Xfer                                                      */
+/*--------------------------------------------------------------------------*/
+int32_t SCMI_MiscEepromXfer(uint32_t channel, uint8_t devId, uint8_t dir, 
+    uint16_t offset, uint8_t* buffer, uint16_t len)
+{
+    int32_t status;
+    uint32_t header;
+    void *msg;
+
+    /* Request message structure */
+    typedef struct
+    {
+        uint32_t header;
+        uint32_t devId;
+        uint32_t dir;
+        uint32_t offset;
+        uint32_t buffer;
+        uint32_t len;
+    } msg_tmiscd12_t;
+
+    /* Acquire lock */
+    SCMI_A2P_LOCK(channel);
+
+    /* Init buffer */
+    status = SCMI_BufInit(channel, &msg);
+    
+    /* Send request */
+    if (status == SCMI_ERR_SUCCESS)
+    {
+        msg_tmiscd12_t *msgTx = (msg_tmiscd12_t *) msg;
+
+        msgTx->devId = (uint32_t)devId;
+        msgTx->dir = (uint32_t)dir;
+        msgTx->buffer = (uint32_t)buffer;
+        msgTx->offset = (uint32_t)offset;
+        msgTx->len = (uint32_t)len;
+
+        /* Send message */
+        status = SCMI_A2pTx(channel, COMMAND_PROTOCOL,
+            SCMI_MSG_MISC_EEPROM_XFER, sizeof(msg_tmiscd12_t), 
+            &header);
+    }
+
+    /* Receive response */
+    if (status == SCMI_ERR_SUCCESS)
+    {
+        /* Reseive message */
+        status = SCMI_A2pRx(channel, sizeof(msg_status_t), header);
+    }
+
+    /* Release lock */
+    SCMI_A2P_UNLOCK(channel);
+
+    /* Return status */
+    return status;
+}
